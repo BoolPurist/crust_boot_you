81686463133bd3d1589d6a46e07b8f0a2609c7b5
BoolPurist <fnaumann@posteo.de>
2023-07-23 11:40:48 +0200

    Integration test: detect error for deletion on non existing template

--- tests/delete_template.rs
+++ tests/delete_template.rs
@@ -22,3 +22,20 @@ fn delete_existing_given_template() {
 
     setup.assert_with_expected();
 }
+#[test]
+#[named]
+fn delete_err_on_non_existing_template() {
+    let setup = TestSetup::new(actual_expected!());
+
+    let given_name = ValidTemplateName::new("to delete".to_string()).unwrap();
+
+    // Act
+    let output = handle_commands::handle_delete_template(
+        setup.path_provider(),
+        setup.os_mani(),
+        &given_name,
+    )
+    .expect_err("Should be an error in this test case");
+    setup.assert_with_expected();
+    insta::assert_display_snapshot!(output);
+}
